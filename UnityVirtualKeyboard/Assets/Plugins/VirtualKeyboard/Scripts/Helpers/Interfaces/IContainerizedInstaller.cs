92355bb6c9b0ea1a00e5464c6086609183bb1b0d
Anton Moiseev <moiseev7@gmail.com>
2019-03-10 10:29:36 +0300

    Added IContainerizedInstaller helper

--- UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/Helpers/Interfaces/IContainerizedInstaller.cs
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/Helpers/Interfaces/IContainerizedInstaller.cs
@@ -1,10 +1,16 @@
-﻿namespace VirtualKeyboard.Scripts.Helpers.Interfaces
+﻿using Zenject;
+
+namespace VirtualKeyboard.Scripts.Helpers.Interfaces
 {
     /// <summary>
     /// Interface for a Zenject Installer with container provided
     /// </summary>
-    public interface IContainerizedInstaller 
+    public interface IContainerizedInstaller
     {
-    
+        /// <summary>
+        /// Install bindings into the container provided as a parameter
+        /// </summary>
+        /// <param name="Container">Container to install bindings to</param>
+        void InstallBindings(DiContainer Container);
     }
 }
