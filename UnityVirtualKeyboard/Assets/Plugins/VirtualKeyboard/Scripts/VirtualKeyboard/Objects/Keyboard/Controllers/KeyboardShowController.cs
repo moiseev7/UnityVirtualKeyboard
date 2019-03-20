04d853253ddaabdfb3ba6985e8d6e93308b1caba
Anton Moiseev <moiseev7@gmail.com>
2019-03-20 21:37:39 +0300

    KeyboardShowController is in progress: - added class

---
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Objects/Keyboard/Controllers/KeyboardShowController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using VirtualKeyboard.Managers.InputFieldManagement.Manager;
+using Zenject;
+
+namespace VirtualKeyboard.Objects.Keyboard.Controllers
+{
+    /// <summary>
+    /// Responsible for showing and hiding the keyboard
+    /// </summary>
+    public class KeyboardShowController : IInitializable, IDisposable
+    {
+        /// <summary>
+        /// Injection fo the controlled game object
+        /// </summary>
+        [Inject(Id = "KeyboardShowController - Controlled Object")]
+        private GameObject _controlledGameObject;
+
+        /// <summary>
+        /// Injection of the input field selection manager
+        /// </summary>
+        [Inject] private IInputFieldSelectionManager _selectionManager;
+
+        /// <summary>
+        /// Disposables related to the object
+        /// </summary>
+        private readonly CompositeDisposable _disposable = new CompositeDisposable();
+
+        public void Initialize()
+        {
+            _disposable.Add(_selectionManager.IsFieldSelectedAsObservable.Subscribe(_controlledGameObject.SetActive));
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+    }
+}
