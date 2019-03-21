3b88efb29cd88ba8424d22437c49c7368775285a
Anton Moiseev <moiseev7@gmail.com>
2019-03-21 21:38:52 +0300

    Added config for the keyboard position controller: - added config - added 2 instances of the config

---
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Objects/Keyboard/Controllers/PositionController/IKeyboardPositionControllerConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VirtualKeyboard.Objects.Keyboard.Controllers.PositionController
+{
+    /// <summary>
+    /// Interface for KeyboardPositionControllerConfig
+    /// </summary>
+    public interface IKeyboardPositionControllerConfig
+    {
+        /// <summary>
+        /// Offset of the keyboard position relative to the position initially intended by the position controller.
+        /// </summary>
+        Vector2 PositionOffset { get; }
+    }
+}
--- UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Objects/Keyboard/Controllers/PositionController/KeyboardPositionControllerConfig.cs
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Objects/Keyboard/Controllers/PositionController/KeyboardPositionControllerConfig.cs
@@ -5,6 +5,7 @@ namespace VirtualKeyboard.Objects.Keyboard.Controllers.PositionController
     /// <summary>
     /// Config for KeyboardPositionController
     /// </summary>
+    [CreateAssetMenu(fileName = "KeyboardPositionControllerConfig", menuName = "Virtual Keyboard/Objects/Controllers/Keyboard Position Controller Config")]
     public class KeyboardPositionControllerConfig : ScriptableObject, IKeyboardPositionControllerConfig
     {
         /// <summary>
@@ -21,16 +22,5 @@ namespace VirtualKeyboard.Objects.Keyboard.Controllers.PositionController
         /// offset (0,-1) would mean that the keyboard will be placed 1 unity bellow the input field
         /// </summary>
         public Vector2 PositionOffset => _positionOffset;
-    }
-
-    /// <summary>
-    /// Interface for KeyboardPositionControllerConfig
-    /// </summary>
-    public interface IKeyboardPositionControllerConfig
-    {
-        /// <summary>
-        /// Offset of the keyboard position relative to the position initially intended by the position controller.
-        /// </summary>
-        Vector2 PositionOffset { get; }
     }
 }
