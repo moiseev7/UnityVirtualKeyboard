a9e514b45631d8d68f7ac3c3cb5b7d92663fe434
Anton Moiseev <moiseev7@gmail.com>
2019-03-22 09:21:50 +0300

    Started to work on KeyboardCanvasController

---
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Objects/Keyboard/Controllers/CanvasController/KeyboardCanvasController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using VirtualKeyboard.Managers.InputFieldManagement.Manager;
+using Zenject;
+
+namespace VirtualKeyboard.Objects.Keyboard.Controllers.CanvasController
+{
+    /// <summary>
+    /// Controller for the keyboard canvas
+    /// </summary>
+    public class KeyboardCanvasController : IInitializable, IDisposable
+    {
+        /// <summary>
+        /// Injection of the controlled canvas
+        /// </summary>
+        [Inject(Id = "KeyboardCanvasController - Controlled Canvas")]
+        private Canvas _controlledCanvas;
+
+        /// <summary>
+        /// Injection of the field selection manager
+        /// </summary>
+        [Inject] private IInputFieldSelectionManager _fieldSelectionManager;
+
+        public void Initialize()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
