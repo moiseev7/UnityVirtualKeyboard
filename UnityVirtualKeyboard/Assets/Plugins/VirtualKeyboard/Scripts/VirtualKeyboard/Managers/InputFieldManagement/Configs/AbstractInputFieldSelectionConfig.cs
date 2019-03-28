b6f60f5fdfb8522a657f310388903ba6bc5ff6a9
Anton Moiseev <a.moiseev@roundme.com>
2019-03-28 08:43:21 +0300

    Moved AbstractInputFieldSelectionConfig to a separate class

---
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Managers/InputFieldManagement/Configs/AbstractInputFieldSelectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace VirtualKeyboard.Managers.InputFieldManagement.Configs
+{
+    public abstract class AbstractInputFieldSelectionConfig : ScriptableObject, IInputFieldSelectionConfig<Selectable>
+    {
+        public abstract bool CheckIfSelected(Selectable selected);
+
+        public abstract bool CheckIfSupportsSubmit(Selectable selected);
+
+        public abstract Texture2D GetSubmitTexture(Selectable selected);
+
+        public abstract TypingDelegate<Selectable> TypingAction { get; }
+        public abstract FieldDelegate<Selectable> DeleteLastSymbolAction { get; }
+        public abstract FieldDelegate<Selectable> SubmitAction { get; }
+    }
+}
--- UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Managers/InputFieldManagement/Configs/IInputFieldSelectionConfig.cs
+++ UnityVirtualKeyboard/Assets/Plugins/VirtualKeyboard/Scripts/VirtualKeyboard/Managers/InputFieldManagement/Configs/IInputFieldSelectionConfig.cs
@@ -1,22 +1,8 @@
 ﻿using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace VirtualKeyboard.Managers.InputFieldManagement.Configs
 {
-    public abstract class AbstractInputFieldSelectionConfig : ScriptableObject, IInputFieldSelectionConfig<Selectable>
-    {
-        public abstract bool CheckIfSelected(Selectable selected);
-
-        public abstract bool CheckIfSupportsSubmit(Selectable selected);
-
-        public abstract Texture2D GetSubmitTexture(Selectable selected);
-
-        public abstract TypingDelegate<Selectable> TypingAction { get; }
-        public abstract FieldDelegate<Selectable> DeleteLastSymbolAction { get; }
-        public abstract FieldDelegate<Selectable> SubmitAction { get; }
-    }
-
     /// <summary>
     ///     Interface for a config that stores reaction to an input field selection
     /// </summary>
